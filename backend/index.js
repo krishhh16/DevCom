366ad07ab4639466bdf99f1ba2bd5c25377341b9
krishh16 <krishh16@yahoo.com>
2023-12-17 18:41:50 +0530

    added some logics

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
     );
-    next()
+    next();
 })
 app.use(express.json())
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import React, {useState} from 'react';
-import './index.css'
 import {
   BrowserRouter as Router,
   Routes,
